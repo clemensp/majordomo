90017dc4230cdddb37adb37a4266aaff4eb2deeb
Clemens Park <clemens.park@gmail.com>
2013-06-14 11:57:11 -0400

    adds the assets new page

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,5 @@
+class AssetsController < ApplicationController
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Majordomo::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 
+  resources :assets
+
   root :to => 'home#index'
 end
