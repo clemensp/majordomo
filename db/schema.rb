9e9ba2c77bb4cef7d549f963fd7f3c9ac3c96452
Syed Hussaini <syed@syedhussaini.ca>
2013-06-14 15:05:18 -0400

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
+
 ActiveRecord::Schema.define(:version => 20130614165353) do
 
   create_table "assets", :force => true do |t|
@@ -19,6 +20,16 @@ ActiveRecord::Schema.define(:version => 20130614165353) do
     t.datetime "updated_at",  :null => false
     t.string   "uuid"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -38,13 +49,4 @@ ActiveRecord::Schema.define(:version => 20130614165353) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 end
