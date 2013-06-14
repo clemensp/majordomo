7880da8628345a0ae41de6e8e0e794e35451cdb3
Lukas Beaton <lukas.beaton@gmail.com>
2013-06-14 16:50:59 -0400

    fixes minor issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable, :registerable, :recoverable, :rememberable, and :omniauthable
-  devise :database_authenticatable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable, :trackable, :validatable, :omniauthablem
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
   def self.find_for_open_id(access_token, signed_in_resource=nil)
