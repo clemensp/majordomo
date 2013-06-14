b02e0f39a9d29e7562f81b75bfeb0b38b42e1024
Lukas Beaton <lukas.beaton@gmail.com>
2013-06-14 15:13:35 -0400

    adds basic authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  # :lockable, :timeoutable, :registerable, :recoverable, :rememberable, and :omniauthable
+  devise :database_authenticatable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -11,7 +10,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_open_id(access_token, signed_in_resource=nil)
     data = access_token['info']
-    if user = User.where(email: data["email"]).first
+    if user = User.where(email: data["email"], name: data["name"]).first
       user
     else # Create a user with a stub password.
       User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
