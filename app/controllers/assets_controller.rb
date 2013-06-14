002eb5bca3a6ec89e70be1b1a8c5cb46cd3a1b75
Clemens Park <clemens.park@gmail.com>
2013-06-14 12:26:51 -0400

    adds CRUD for assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,19 @@
 class AssetsController < ApplicationController
-  def new
+  def index
+    @assets = Asset.all
+  end
 
+  def show
+    @asset = Asset.find(params[:id])
+  end
+
+  def new
+    @asset = Asset.new
+  end
+
+  def create
+    @asset = Asset.create(params[:asset])
+
+    redirect_to asset_path(@asset)
   end
 end
---
+++ app/models/asset.rb
@@ -0,0 +1,3 @@
+class Asset < ActiveRecord::Base
+  attr_accessible :name, :description
+end
