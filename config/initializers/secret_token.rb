b236d5560ed3bf8a4fc78b1373de66248475db40
Clemens Park <clemens.park@gmail.com>
2013-06-14 10:55:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Majordomo::Application.config.secret_token = '48056ef12f1c33d87a48d28976947b5ecd95078b683b30d8900e23b1c9ab85771337031f9b8b9a71227c06e53726a6ffd6be0e531ee4369443cb9b50ee01a603'
