fbf8a55e0d297f93414db6acd8f5cd835cf3a047
Clemens Park <clemens.park@gmail.com>
2013-07-19 17:08:03 -0400

    adds crud.css for asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Majordomo::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w( basement.css )
+  config.assets.precompile += %w( basement.css crud.css)
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
